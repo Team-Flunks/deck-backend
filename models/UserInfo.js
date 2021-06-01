22c1d2fcf74fb1ae191f0183d2dfe032ef97247d
Keian <KeianAnthony1@gmail.com>
2021-06-01 16:21:12 -0700

    formatting changes

--- models/UserInfo.js
+++ models/UserInfo.js
@@ -3,20 +3,19 @@
 const mongoose = require('mongoose');
 
 const GameSchema = new mongoose.Schema({
-  game: {type: String, required: true},
-  timesPlayed: {type: Number},
-  timesWon: {type: Number},
-  highscore:{type: Number}
+  game: { type: String, required: true },
+  timesPlayed: { type: Number },
+  timesWon: { type: Number },
+  highscore: { type: Number }
 });
-
 
 const GameModel = mongoose.model('Games', GameSchema);
 
 const UserSchema = new mongoose.Schema({
-  email: {type: String, required:true},
+  email: { type: String, required: true },
   gameRecords: [GameSchema]
 });
 
 const UserModel = mongoose.model('Players', UserSchema);
 
-module.exports = {UserModel, GameModel};
+module.exports = { UserModel, GameModel };
