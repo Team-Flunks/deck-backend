158a6eb56967a038cad3544554845df3f125e77a
Charles Bofferding <chaboffe@gmail.com>
2021-06-01 17:28:47 -0700

    Modularization and code clean-up

--- models/UserInfo.js
+++ models/UserInfo.js
@@ -1,7 +1,9 @@
 'use strict';
 
+//=============== Setup ===============
 const mongoose = require('mongoose');
 
+//=============== Game Schema ===============
 const GameSchema = new mongoose.Schema({
   game: { type: String, required: true },
   timesPlayed: { type: Number },
@@ -11,11 +13,14 @@ const GameSchema = new mongoose.Schema({
 
 const GameModel = mongoose.model('Games', GameSchema);
 
+//=============== User Schema ===============
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  gameRecords: [GameSchema]
+  gameRecords: [GameSchema],
+  tokenCount: { type: Number }
 });
 
 const UserModel = mongoose.model('Players', UserSchema);
 
+//=============== Exports ===============
 module.exports = { UserModel, GameModel };
--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 //===============Importing and setting up===============
 //Loading in Libraries
-require('dotenv').config();
+const mongoose = require('mongoose');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
-const mongoose = require('mongoose');
+require('dotenv').config();
 
 //App setup
 const express = require ('express');
@@ -15,13 +15,10 @@ app.use(cors());
 app.use(express.json());
 
 //Created files
-const UserInfo=require('./models/UserInfo.js');
-// const GameModel=require('./models/UserInfo.js');
-const UserModel = require('./models/UserInfo.js');
+const UserInfo = require('./models/UserInfo.js');
+const UserDatabase = require('./modules/UserDatabase.js');
 const pokeInfoRequest = require('./modules/PokeAPI.js');
-const verifyToken = require('./modules/verification.js');
-
-const getBooleanFacts=require('./modules/KnowledgeAPI.js');
+const getBooleanFacts = require('./modules/KnowledgeAPI.js');
 
 
 //===============Mongoose database setup===============
@@ -66,140 +63,16 @@ db.once('open', function () {
 //Refer to Modules/PokeAPI for info on what is happening here
 app.get('/pokeInfo', pokeInfoRequest);
 
-// GET single user info
-//Connection made via Auth0 email
-app.get('/singleUser', (req, res) => {
-  
-  // Grab and verify token
-  console.log('This is the req information for single user');
-  console.log(req.headers);
-  const token = req.headers.authorization.split(' ')[1];
-  verifyToken(token, singleUserData);
+//Refer to Modules/UserDatabase for info on what is happening here
+app.get('/singleUser', UserDatabase.singleUser);
 
-  //Find stored data of verified user
-  //Pass it into verifyToken so we can use Auth0 things inside this function
-  async function singleUserData(user) {
-    //Legacy variable names, edit here
-    let Users = await UserInfo.UserModel.find({ email: user.email });
-    console.log("Server Side return of Single User Pull");
-    console.log(Users[0]);
-    //if the user isn't in DB yet maybe create a new user with the info passed in here
-    if (!Users.length) {
-      console.log("Server Side Single User Pull Found Nothing");
-      res.send([]);
-    }
-    //Send the data out
-    res.send(Users[0]);
-  }
-});
+//Refer to Modules/UserDatabase for info on what is happening here
+app.get('/allUsers', UserDatabase.allUsers);
 
-// GET single user info
-//Connection made via Auth0 email
-app.get('/allUsers', (req, res) => {
-  
-  // Grab and verify token
-  const token = req.headers.authorization.split(' ')[1];
-  verifyToken(token, allUserData);
+//Refer to Modules/UserDatabase for info on what is happening here
+app.put('/updateUser', UserDatabase.updateUser);
 
-  //Find stored data of verified user
-  //Pass it into verifyToken so we can use Auth0 things inside this function
-  async function allUserData(user) {
-    //Legacy variable names, edit here
-    let Users = await UserInfo.UserModel.find({ });
-    console.log("Server Side return of Single User Pull")
-    console.log(Users);
-    //if the user isn't in DB yet maybe create a new user with the info passed in here
-    if (!Users.length) {
-      console.log("Server Side All User Pull Found Nothing")
-      res.send([]);
-    }
-    //Send the data out
-    res.send(Users);
-  }
-});
-
-
-app.put('/updateUser', (request,response) => {
-  console.log('Update User Information coming in from client side');
-  console.log(request.params, request.query)
-
-  const token = request.headers.authorization.split(' ')[1];
-  verifyToken(token, updateUserScore);
-
- 
-  async function updateUserScore(user) {
-    const gameIn = request.query.game
-    const didWinIn = parseInt(request.query.didWin);
-    const scoreIn = parseInt(request.query.score);
-    const emailIn = user.email;
-
-
-    await UserInfo.UserModel.find({ email: emailIn }, (err, person) => {
-      if (err) console.log(err);
-      if(person.length == 0) {
-        const newUser = new UserInfo.UserModel ({
-          email: emailIn
-        });
-        person.push(newUser);
-      }
-      
-      // For loop to check if person has played and seeing if record matches this data
-      let tempIndex = -1;
-      for( let g = 0; g < person[0].gameRecords.length; g ++) {
-        if(person[0].gameRecords[g].game === gameIn) {
-          tempIndex = g;
-        }
-      }
-      
-      // If the game isn't new brining in newGameRecord and plugging in different data
-      if(tempIndex > -1){
-        // This is where the logic for updating happens
-
-        // Setting newTimesWon to increase if did win game 
-        let newTimesWon = person[0].gameRecords[tempIndex].timesWon;
-        if(didWinIn){
-          // If does not increment then this is the reason
-          newTimesWon ++;
-        }
-
-        // Setting scoreIn to change to new high score if it greater than the previous high score
-        let newHighScore = person[0].gameRecords[tempIndex].highscore;
-        if(scoreIn > newHighScore) {
-          newHighScore = scoreIn;
-        }
-
-        // This portion is in relation to the update of the new values
-        person[0].gameRecords[tempIndex].timesPlayed = person[0].gameRecords[tempIndex].timesPlayed + 1;
-        person[0].gameRecords[tempIndex].timesWon = newTimesWon;
-        person[0].gameRecords[tempIndex].highscore = newHighScore;
-      } else {
-
-        // Setting the record of a new game 
-        // Setting newTimesWon to increase if did win game 
-        let newTimesWon = 0;
-        if(didWinIn){
-          // If does not increment then this is the reason
-          newTimesWon ++;
-        }
-        // Making new game record
-        const newGameRecord = new UserInfo.GameModel ({
-          game: gameIn,
-          timesPlayed: 1,
-          timesWon: newTimesWon,
-          highscore: scoreIn
-        });
-        person[0].gameRecords.push(newGameRecord);
-      }
-      person[0].save();
-
-      // Might need to change response.send back if front end wants different info
-      response.send(person[0].gameRecords);
-    })
-  }
-});
-
-
-// Specific route for knowledge game with function
+//Refer to Modules/Knowledge for info on what is happening here
 app.get('/knowledgegame', getBooleanFacts);
 
 
